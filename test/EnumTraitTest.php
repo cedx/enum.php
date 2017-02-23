a9602c9057987e8a25d8de92ecdf816b1ec62716
Cédric Belin <cedric@belin.io>
2017-02-24 00:16:55 +0100

    Using BDD specifications for the unit tests

--- test/EnumTraitTest.php
+++ test/EnumTraitTest.php
@@ -5,6 +5,7 @@
 namespace cedx\test;
 
 use cedx\{EnumTrait};
+use Codeception\{Specify};
 use PHPUnit\Framework\{TestCase};
 
 /**
@@ -38,71 +39,80 @@ final class SampleEnum {
  * @coversDefaultClass \cedx\EnumTrait
  */
 class EnumTraitTest extends TestCase {
+  use Specify;
 
   /**
    * @test ::__construct
    */
   public function testConstructor() {
-    $constructor = (new \ReflectionClass(SampleEnum::class))->getConstructor();
-
-    // Should create types that are not instantiable.
-    $this->assertTrue($constructor->isFinal());
-    $this->assertTrue($constructor->isPrivate());
+    $this->specify('should create types that are not instantiable', function() {
+      $constructor = (new \ReflectionClass(SampleEnum::class))->getConstructor();
+      $this->assertTrue($constructor->isFinal());
+      $this->assertTrue($constructor->isPrivate());
+    });
   }
 
   /**
    * @test ::isDefined
    */
   public function testIsDefined() {
-    // Should return `false` for unknown values.
-    $this->assertFalse(SampleEnum::isDefined('TWO'));
-    $this->assertFalse(SampleEnum::isDefined(3.1));
+    $this->specify('should return `false` for unknown values', function() {
+      $this->assertFalse(SampleEnum::isDefined('TWO'));
+      $this->assertFalse(SampleEnum::isDefined(3.1));
+    });
 
-    // Should return `true` for known values.
-    $this->assertTrue(SampleEnum::isDefined(false));
-    $this->assertTrue(SampleEnum::isDefined(1));
-    $this->assertTrue(SampleEnum::isDefined('two'));
-    $this->assertTrue(SampleEnum::isDefined(3.0));
+    $this->specify('should return `true` for known values', function() {
+      $this->assertTrue(SampleEnum::isDefined(false));
+      $this->assertTrue(SampleEnum::isDefined(1));
+      $this->assertTrue(SampleEnum::isDefined('two'));
+      $this->assertTrue(SampleEnum::isDefined(3.0));
+    });
 
-    // Should return `false` for similar values in strict mode.
-    $this->assertFalse(SampleEnum::isDefined('', true));
-    $this->assertFalse(SampleEnum::isDefined(1.0, true));
-    $this->assertFalse(SampleEnum::isDefined(3, true));
+    $this->specify('should return `false` for similar values in strict mode', function() {
+      $this->assertFalse(SampleEnum::isDefined('', true));
+      $this->assertFalse(SampleEnum::isDefined(1.0, true));
+      $this->assertFalse(SampleEnum::isDefined(3, true));
+    });
   }
 
   /**
    * @test ::getName
    */
   public function testGetName() {
-    // Should return an empty string for unknown values.
-    $this->assertEquals('', SampleEnum::getName('TWO'));
-    $this->assertEquals('', SampleEnum::getName(3.1));
+    $this->specify('should return an empty string for unknown values', function() {
+      $this->assertEquals('', SampleEnum::getName('TWO'));
+      $this->assertEquals('', SampleEnum::getName(3.1));
+    });
 
-    // Should return the name for known values.
-    $this->assertEquals('ZERO', SampleEnum::getName(false));
-    $this->assertEquals('ONE', SampleEnum::getName(1));
-    $this->assertEquals('TWO', SampleEnum::getName('two'));
-    $this->assertEquals('THREE', SampleEnum::getName(3.0));
+    $this->specify('should return the name for known values', function() {
+      $this->assertEquals('ZERO', SampleEnum::getName(false));
+      $this->assertEquals('ONE', SampleEnum::getName(1));
+      $this->assertEquals('TWO', SampleEnum::getName('two'));
+      $this->assertEquals('THREE', SampleEnum::getName(3.0));
+    });
 
-    // Should return an empty string for similar values in strict mode.
-    $this->assertEquals('', SampleEnum::getName(0, true));
-    $this->assertEquals('', SampleEnum::getName(1.0, true));
-    $this->assertEquals('', SampleEnum::getName(3, true));
+    $this->specify('should return an empty string for similar values in strict mode', function() {
+      $this->assertEquals('', SampleEnum::getName(0, true));
+      $this->assertEquals('', SampleEnum::getName(1.0, true));
+      $this->assertEquals('', SampleEnum::getName(3, true));
+    });
   }
 
   /**
    * @test ::getNames
    */
   public function testGetNames() {
-    // Should return the names of the enumerable properties.
-    $this->assertEquals(['ZERO', 'ONE', 'TWO', 'THREE'], SampleEnum::getNames());
+    $this->specify('should return the names of the enumerable properties', function() {
+      $this->assertEquals(['ZERO', 'ONE', 'TWO', 'THREE'], SampleEnum::getNames());
+    });
   }
 
   /**
    * @test ::getValues
    */
   public function testGetValues() {
-    // Should return the values of the enumerable properties.
-    $this->assertEquals([false, 1, 'two', 3.0], SampleEnum::getValues());
+    $this->specify('should return the values of the enumerable properties', function() {
+      $this->assertEquals([false, 1, 'two', 3.0], SampleEnum::getValues());
+    });
   }
 }
