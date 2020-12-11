6888665ab5b8f3449859a007c8aedb8cb028d9eb
Cédric Belin <cedric@belin.io>
2020-12-11 18:26:28 +0100

    Using the `mixed` type

--- src/EnumTrait.php
+++ src/EnumTrait.php
@@ -9,81 +9,53 @@ trait EnumTrait {
 
 	/**
 	 * Returns the specified value if it exists in this enumeration, otherwise throws an exception.
-	 * @param mixed $value The value of a constant in this enumeration.
-	 * @return mixed The specified enumerated constant.
 	 * @throws \UnexpectedValueException No such constant was found.
 	 */
-	final static function assert($value) {
-		if (static::isDefined($value)) return $value;
-		throw new \UnexpectedValueException("Invalid enumerated value: $value");
+	final static function assert(mixed $value): mixed {
+		return static::isDefined($value) ? $value : throw new \UnexpectedValueException("Invalid enumerated value: $value");
 	}
 
-	/**
-	 * Returns the specified value if it exists in this enumeration, otherwise returns the given default value.
-	 * @param mixed $value The value of a constant in this enumeration.
-	 * @param mixed $defaultValue The default value to return if the specified constant does not exist.
-	 * @return mixed The specified enumerated constant, or the default value if no such constant is found.
-	 */
-	final static function coerce($value, $defaultValue = null) {
+	/** Returns the specified value if it exists in this enumeration, otherwise returns the given default value. */
+	final static function coerce(mixed $value, mixed $defaultValue = null): mixed {
 		return static::isDefined($value) ? $value : $defaultValue;
 	}
 
-	/**
-	 * Gets an indication whether a constant with a specified value exists in this enumeration.
-	 * @param mixed $value The value of a constant in this enumeration.
-	 * @return bool `true` if a constant in this enumeration has the specified value, otherwise `false`.
-	 */
-	final static function isDefined($value): bool {
+	/** Gets an indication whether a constant with a specified value exists in this enumeration. */
+	final static function isDefined(mixed $value): bool {
 		return in_array($value, static::getValues(), true);
 	}
 
-	/**
-	 * Gets an associative array of the pairs of names and values of the constants in this enumeration.
-	 * @return array<string, mixed> An associative array that contains the pairs of names and values of the constants in this enumeration.
-	 */
+	/** Gets an associative array of the pairs of names and values of the constants in this enumeration. */
 	final static function getEntries(): array {
 		static $entries;
-		$entries ??= array_filter(
-			(new \ReflectionClass(static::class))->getConstants(),
-			fn($name) => (new \ReflectionClassConstant(static::class, $name))->isPublic(),
-			ARRAY_FILTER_USE_KEY
-		);
-
+		$entries ??= (new \ReflectionClass(static::class))->getConstants(\ReflectionClassConstant::IS_PUBLIC);
 		return $entries;
 	}
 
 	/**
 	 * Gets the zero-based position of the constant in this enumeration that has the specified value.
-	 * @param mixed $value The value of a constant in this enumeration.
-	 * @return int The zero-based position of the constant that has the specified value, or `-1` if no such constant is found.
+	 * Returns `-1` if no such constant is found.
 	 */
-	final static function getIndex($value): int {
+	final static function getIndex(mixed $value): int {
 		$index = array_search($value, static::getValues(), true);
-		return $index !== false ? $index : -1;
+		return $index !== false ? (int) $index : -1;
 	}
 
 	/**
 	 * Gets the name of the constant in this enumeration that has the specified value.
-	 * @param mixed $value The value of a constant in this enumeration.
-	 * @return string A string containing the name of the constant that has the specified value, or an empty string if no such constant is found.
+	 * Returns an empty string if no such constant is found.
 	 */
-	final static function getName($value): string {
+	final static function getName(mixed $value): string {
 		$index = static::getIndex($value);
 		return $index >= 0 ? static::getNames()[$index] : "";
 	}
 
-	/**
-	 * Gets an array of the names of the constants in this enumeration.
-	 * @return string[] An array that contains the names of the constants in this enumeration.
-	 */
+	/** Gets an array of the names of the constants in this enumeration. */
 	final static function getNames(): array {
 		return array_keys(static::getEntries());
 	}
 
-	/**
-	 * Gets an array of the values of the constants in this enumeration.
-	 * @return mixed[] An array that contains the values of the constants in this enumeration.
-	 */
+	/** Gets an array of the values of the constants in this enumeration. */
 	final static function getValues(): array {
 		return array_values(static::getEntries());
 	}
